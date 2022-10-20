6f462ac9ddc1e58c7133a24a3c1f257a156d6067
John Doe <cformation75@gmail.com>
2022-10-20 21:44:44 +0200

    mm

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -14,6 +14,13 @@ class Portfolio extends Component {
         <div key={id++} className="columns portfolio-item">
           <div className="item-wrap">
             <Zmage alt={projects.title} src={projectImage} />
+          
+              <button className="submit">Submit</button>
+              <span id="image-loader">
+                <img alt="" src="images/loader.gif" />
+              </span>
+            
+
             <div style={{ textAlign: "center" }}>{projects.title}</div>
           </div>
         </div>
